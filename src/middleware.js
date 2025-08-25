e5d648e2f1d7fe8dbace4001668ba296501d2785
Duynb01 <bduy0001@gmail.com>
2025-08-25 14:08:49 +0700

    Return ver Done fix voucher placeholder

--- src/middleware.js
+++ src/middleware.js
@@ -2,39 +2,28 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request) {
-  const { pathname } = request.nextUrl;
+  // const token = request.cookies.get("access_token")?.value;
+  const token = request.nextUrl.searchParams.get("token");
+  // const test = localStorage.getItem("access_token");
 
-  if (pathname.startsWith("/dashboard")) {
-    const token = request.cookies.get("access_token")?.value;
-    if (!token) {
-      console.log("CheckToken: ", token);
+  // console.log("Check token1: ", token);
+  // console.log("Check token2: ", test);
+  if (!token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
-      // return NextResponse.redirect(new URL("/", request.url));
-    }
+  try {
+    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
+    const { payload } = await jwtVerify(token, secret);
 
-    try {
-      // Gọi API BE để xác minh token
-      const response = await fetch(
-        "https://api-furniture-x2jh.onrender.com/auth/verify-token",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Cookie: `token=${token}`, // Gửi cookie
-          },
-          credentials: "include", // Gửi cookie cross-domain
-        }
-      );
-      const data = await response.json();
-      if (!response.ok || data.role !== "ADMIN") {
-        return NextResponse.redirect(new URL("/unauthorized", request.url));
-      }
-    } catch (error) {
-      console.error("Error verifying token:", error);
+    if (payload.role !== "ADMIN") {
       return NextResponse.redirect(new URL("/unauthorized", request.url));
     }
+
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  return NextResponse.next();
 }
 
 export const config = {
