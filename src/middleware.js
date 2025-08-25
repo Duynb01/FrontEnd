0d9425d340a1ceba9e7f6bbb5f833a6beefa70f8
Duynb01 <bduy0001@gmail.com>
2025-08-25 13:08:30 +0700

    test middleware v1

--- src/middleware.js
+++ src/middleware.js
@@ -2,28 +2,39 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request) {
-  // const token = request.cookies.get("access_token")?.value;
-  const token = request.nextUrl.searchParams.get("token");
-  // const test = localStorage.getItem("access_token");
+  const { pathname } = request.nextUrl;
 
-  // console.log("Check token1: ", token);
-  // console.log("Check token2: ", test);
-  if (!token) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  if (pathname.startsWith("/dashboard")) {
+    const token = request.cookies.get("access_token")?.value;
+    if (!token) {
+      console.log("CheckToken: ", token);
 
-  try {
-    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
-    const { payload } = await jwtVerify(token, secret);
-
-    if (payload.role !== "ADMIN") {
-      return NextResponse.redirect(new URL("/unauthorized", request.url));
+      // return NextResponse.redirect(new URL("/", request.url));
     }
 
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.redirect(new URL("/", request.url));
+    try {
+      // Gọi API BE để xác minh token
+      const response = await fetch(
+        "https://api-furniture-x2jh.onrender.com/auth/verify-token",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: `token=${token}`, // Gửi cookie
+          },
+          credentials: "include", // Gửi cookie cross-domain
+        }
+      );
+      const data = await response.json();
+      if (!response.ok || data.role !== "ADMIN") {
+        return NextResponse.redirect(new URL("/unauthorized", request.url));
+      }
+    } catch (error) {
+      console.error("Error verifying token:", error);
+      return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
   }
+  return NextResponse.next();
 }
 
 export const config = {
