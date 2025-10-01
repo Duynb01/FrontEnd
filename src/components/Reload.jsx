3a1947eae178758b196d1e81146bf4ec3964e386
Duynb01 <bduy0001@gmail.com>
2025-10-01 19:21:56 +0700

    Test Update v2

--- src/components/Reload.jsx
+++ src/components/Reload.jsx
@@ -1,20 +1,22 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCheckLogin, setLogout } from "@/redux/store/slices/authSlice";
 import { reloadUser } from "@/lib/api/apiAuth";
-import { Loader } from "lucide-react";
+// import { Loader } from "lucide-react";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
 import { deleteProfile, setProfile } from "@/redux/store/slices/userSlice";
 export default function Reload({ children }) {
   const router = useRouter();
   const dispatch = useDispatch();
+  // const [loading, setLoading] = useState(true);
   const isLogin =
     typeof window !== "undefined" && localStorage.getItem("isLogin");
   useEffect(() => {
     if (!isLogin) {
       dispatch(setLogout());
+      // setLoading(false);
       return;
     }
     const fetchReload = async () => {
@@ -34,10 +36,19 @@ export default function Reload({ children }) {
         dispatch(deleteProfile());
         router.push("/");
       } finally {
+        // setLoading(false);
       }
     };
     fetchReload();
   }, [dispatch]);
+
+  // if (loading) {
+  //   return (
+  //     <div className="w-screen h-screen flex items-center justify-center">
+  //       <Loader className="w-6 h-6 animate-spin text-main" />
+  //     </div>
+  //   );
+  // }
 
   return <>{children}</>;
 }
